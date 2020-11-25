ad455d279136ab7099543b4009abe87d40ee3261
sbedoyac1 <sbedoyac1@eafit.edu.co>
2020-11-25 00:55:34 -0500

    About

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 # ----------------------------------------------------- Juan Escudero -----------------------------------------------------
 
 @app.route("/direct_methods")
